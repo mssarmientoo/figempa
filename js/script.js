bf853a874f07b54c760630b1664fca687ead4978
mssarmientoo <mssarmiento@uce.edu.ec>
2025-11-03 01:01:56 -0500

    Changing and improving website

---
+++ js/script.js
@@ -0,0 +1,184 @@
+const body = document.querySelector("body");
+const homeLink = document.querySelector("#home-link");
+const aboutProyectLink = document.querySelector("#about-proyect-link");
+const descriptiveStatisticsLink = document.querySelector("#descriptive-statistics-link");
+const underConstructionLinks = document.querySelectorAll(".under-construction");
+const mainElement = document.querySelector("main");
+const header = document.querySelector(".header");
+const menuBtn = document.querySelector("#menu-btn");
+const topBar = document.querySelector("#top-bar");
+const middleBar = document.querySelector("#middle-bar");
+const bottomBar = document.querySelector("#bottom-bar");
+const navbar = document.querySelector("#navbar");
+const navLinks = document.querySelectorAll(".navlink");
+const navlProjectLinks = document.querySelectorAll(".navl-project");
+const navlDSLinks = document.querySelectorAll(".navl-ds");
+const heroVideoWrap = document.querySelector(".hero-video-wrap");
+
+// Hamburguer menu code
+const toggleHamburguerMenuClases = () => {
+  topBar.classList.toggle("convert-x-1");
+  topBar.classList.toggle("convert-bars");
+  middleBar.classList.toggle("hidden");
+  middleBar.classList.toggle("visible");
+  bottomBar.classList.toggle("convert-x-2");
+  bottomBar.classList.toggle("convert-bars");
+  navbar.classList.toggle("show-menu");
+  body.classList.toggle("block-overflow");
+};
+
+menuBtn.addEventListener("click", () => {
+  toggleHamburguerMenuClases();
+});
+
+navLinks.forEach(link => (
+  link.addEventListener("click", () => {
+    toggleHamburguerMenuClases();
+  })
+));
+
+heroVideoWrap.setAttribute("style", `margin-top:${header.offsetHeight}px`);
+
+// Adding new text to main content
+const homeCode = `
+  <section id="inicio" class="hero-video-wrap section-anchor" role="region" aria-label="Portada con video">
+    <video autoplay muted loop id="heroVideo" class="hero-video">
+      <source src="assets/videos/uce.mp4" type="video/mp4">
+      Tu navegador no soporta el tag de video.
+    </video>
+
+    <div class="video-overlay"></div>
+
+    <div class="hero-inner-centered">
+      <h1 data-i18n="hero_title_centered" class="hero-h1">ANÁLISIS ESTADÍSTICO DE PLANTAS SOLARES A NIVEL MUNDIAL</h1>
+      <p class="lead" data-i18n="hero_lead_centered">Análisis estadístico de datos, aplicando métodos descriptivos e inferenciales con el uso del lenguaje R en RStudio</p>
+    </div>
+  </section>
+
+  <section class="important section-box">
+    <h2 class="title-2">¡IMPORTANTE!</h2>
+    <p>Este proyecto tiene un carácter exclusivamente académico y se encuentra en continuo desarrollo. Los datos y análisis presentados se utilizan únicamente con fines educativos.</p>
+  </section>
+
+  <section class="mision-vision section-box">
+    <h2 class="title-2">MISIÓN - VISIÓN</h2>
+    <div class="mision-vision-container">
+        <div class="mision">
+            <h3 class="title-3">Misión</h3>
+            <p>Aplicar métodos estadísticos para identificar y cuantificar los factores que determinan la eficiencia real de las plantas solares a nivel mundial.</p>
+        </div>
+        <div class="vision">
+            <h3 class="title-3">Visión</h3>
+            <p>Convertirnos en un análisis estadístico clave que revele las verdaderas tendencias y el potencial global de la energía solar.</p>
+        </div>
+    </div>
+  </section>
+
+  <section id="motivation" class="motivation section-box">
+    <div class="card-box">
+      <div class="motivation-text-container">
+        <h2 class="title-2">MOTIVACIONES DEL PROYECTO</h2>
+        <p data-i18n="motivaciones_text">Como estudiantes de la Universidad Central del Ecuador, desarrollamos este proyecto con el propósito de aplicar la estadística como una herramienta fundamental en el análisis de datos. Nuestro objetivo es no solo interpretar la información, sino también destacar la relevancia de la estadística en la toma de decisiones informadas dentro del ámbito profesional relacionado con nuestra carrera.</p>
+        <a href="https://www.uce.edu.ec/web/figempa" target="_blank" style="font-weight:700" class="uce-figempa-link">UNIVERSIDAD CENTRAL DEL ECUADOR</a>
+      </div>
+      <img src="assets/images/facultad.jpg" alt="Edificio de la Facultad de Ingeniería en Geología, Minas, Petróleos y Ambiental." class="motivation-img"/>
+    </div>
+  </section>
+`;
+
+const aboutProyectCode = `
+  <section class="objectives section-box" id="objectives">
+    <h2 class="title-2">OBJETIVOS</h2>
+    <h3 class="title-3">Objetivo General:</h3> 
+    <p class="general-objective">Aplicar la estadística y Machine learning para examinar las plantas solares a nivel mundial, mediante el uso de herramientas computacionales. </p>
+    <h3 class="title-3">Objetivos especificos:</h3>
+    <ol class="objectives-list">
+        <li class="objectives-items">Conocer la situación actual de las plantas solares a nivel mundial a través de sus Caracteristicas mas importantes y medidas estadísticas.</li>
+        <li class="objectives-items">Emplear un modelo de probabilidad para establecer conclusiones sobre las plantas solares en el mundo a partir de los resultados de muestra.</li>
+        <li class="objectives-items">Deducir relaciones entre variables de las plantas solares a nivel mundial con el fin de realizar estimaciones.</li>
+    </ol>
+  </section>
+
+  <section class="problem section-box" id="problem">
+    <h2 class="title-2">PLANTEAMIENTO DEL PROBLEMA</h2>
+    <p class="problem-desc">El análisis estadístico de las plantas solares a nivel mundial es clave para medir su eficiencia real. Este proyecto utilizará una base de datos propia para transformar la información bruta de las plantas (como su capacidad y ubicación) en insumos relevantes. El objetivo es aplicar la estadística para entender qué factores determinan de verdad el rendimiento energético a escala global.</p>
+    <p class="problem-desc">La información utilizada para este estudio fue obtenida desde la plataforma Kaggle, NASA Power Data y Global Solar Atlas, cumpliendo con los criterios establecidos para el proyecto.</p>
+  </section>
+
+  <div class="map-container section-box">
+    <img src="assets/images/map.jpg" alt="mapamundi" class="map-img">
+  </div>
+`;
+
+const descriptiveStatisticsCode = `
+  <section class="section-box" id="descriptive-statistics">
+    <h2 class="title-2">Estadistica descriptiva</h2>
+    <p>Aquí se mostrará el análisis exploratorio: conteos, distribuciones, histogramas, y tablas resumen por región y tipo de planta.</p>
+  </section>
+
+  <section class="section-box" id="ds-methodology">
+    <h1 class="title-2">Metodología</h1>
+    <div class="met-container">
+      <article>
+        <h2 class="title-3">Población</h2>
+        <p>Es el conjunto total de elementos o individuos que comparten una o varias características y sobre los cuales se desea obtener información.</p>
+      </article>
+      <article>
+        <h2 class="title-3">Individuo</h2>
+        <p>El individuo (también llamado unidad estadística) es cada uno de los elementos que forman parte de la población y sobre los cuales se realiza la observación o medición.</p>
+      </article>
+    </div>
+  </section>
+`;
+
+const underConstructionCode = `
+  <h1 class="title-1">En construcción</h1>
+  <img src="assets/images/under-construction.png" alt="Escena de construcción" class="under-construction-img">
+`;
+
+homeLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  mainElement.innerHTML = "";
+  mainElement.innerHTML = homeCode;
+});
+
+aboutProyectLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  mainElement.setAttribute("style", `margin-top:${header.offsetHeight + 10}px`);
+  mainElement.innerHTML = "";
+  mainElement.innerHTML = aboutProyectCode;
+});
+
+navlProjectLinks.forEach(link => {
+  link.addEventListener("click", (e) => {
+    mainElement.setAttribute("style", `margin-top:${header.offsetHeight + 10}px`);
+    mainElement.innerHTML = "";
+    mainElement.innerHTML = aboutProyectCode;
+    // console.log(e.y - (header.offsetHeight + 10));
+    // console.log(e);
+  })
+});
+
+descriptiveStatisticsLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  mainElement.setAttribute("style", `margin-top:${header.offsetHeight + 10}px`);
+  mainElement.innerHTML = "";
+  mainElement.innerHTML = descriptiveStatisticsCode;
+});
+
+navlDSLinks.forEach(link => {
+  link.addEventListener("click", (e) => {
+    mainElement.setAttribute("style", `margin-top:${header.offsetHeight + 10}px`);
+    mainElement.innerHTML = "";
+    mainElement.innerHTML = descriptiveStatisticsCode;
+  })
+});
+
+underConstructionLinks.forEach(link => (
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    mainElement.setAttribute("style", `margin-top:${header.offsetHeight + 10}px`);
+    mainElement.innerHTML = "";
+    mainElement.innerHTML = underConstructionCode;
+  })
+));
